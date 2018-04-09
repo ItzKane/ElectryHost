1202ad8b9a44ed01ee36b30fe686e0ba0ad6ec14
ItzKane <ItzKaneYT@gmail.com>
2018-04-09 20:53:31 +0200

    Update ElectryHost.js

--- ElectryHost.js
+++ ElectryHost.js
@@ -280,7 +280,7 @@ if(message.content.startsWith(".count")){
     "https://discord.gg/sGEQq5E (ElectryHost)",
     "https://discord.gg/VHYFuUD (Discord Bot Development)",
     "https://discord.gg/4GCPzen (Electry Development)",
-		"https://discord.gg/9TgNWAD (BuildersUnited)"
+    "https://discord.gg/9TgNWAD (BuildersUnited)"
 ];
 
 if(message.content === `.battery`) {
@@ -295,21 +295,21 @@ if(message.content === `.reload`) {
     loadCmds();
     }
 
-		if(message.content.startsWith(".promo")){
-		    message.delete(1);
-		    const member = message.member;
-		    const PromoEmbed = new Discord.RichEmbed()
-		    .setTitle(`Promotie`)
-		    .addField('Gebruiker:', `<@${message.author.id}>`)
-		    .addField('Doorgever:', `<@427857462947872779>`)
-		    .addField('Server:', links[Math.floor(Math.random() * links.length)])
-		    .setThumbnail(message.author.displayAvatarURL)
-		    .setFooter('Also promotion? Buy it on https://www.paypal.me/SmikkelHost')
-		    .setTimestamp()
-		    .setColor('0x4628d0')
-		    console.log('PROMO: ELECTRYHOST!')
-		    message.channel.send(PromoEmbed)
-		}
+if(message.content.startsWith(".promo")){
+    message.delete(1);
+    const member = message.member;
+    const PromoEmbed = new Discord.RichEmbed()
+    .setTitle(`Promotie`)
+    .addField('Gebruiker:', `<@${message.author.id}>`)
+    .addField('Doorgever:', `<@427857462947872779>`)
+    .addField('Server:', links[Math.floor(Math.random() * links.length)])
+    .setThumbnail(message.author.displayAvatarURL)
+    .setFooter('Also promotion? Buy it on https://www.paypal.me/SmikkelHost')
+    .setTimestamp()
+    .setColor('0x4628d0')
+    console.log('PROMO: ELECTRYHOST!')
+    message.channel.send(PromoEmbed)
+}
 
 if(message.content.startsWith(".promotie")){
     message.delete(1);
@@ -324,7 +324,7 @@ if(message.content.startsWith(".promotie")){
     .setTimestamp()
     .setColor('0x4628d0')
     console.log('PROMO: ELECTRYHOST!')
-    message.channel.send(PromotieEmbed)
+    message.channel.send(PromotieEmbed);
 }
 
 if(message.content == ".help") {
